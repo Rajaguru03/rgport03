2226ac411323d15ae7c170fe462c54278cb691cc
Rajaguru03 <rajagurun003@gmail.com>
2024-09-16 08:20:37 +0530

    commit

--- src/WebUI/Root.jsx
+++ src/WebUI/Root.jsx
@@ -27,12 +27,15 @@ function Root() {
 
               <div className="container profileName">
                 <h1>Rajaguru Nataraj</h1>
+                <div className='typed-text'>
+                <p>Web~</p>
                 <Typed
-                  className='typed-text'
-                  text={["Web Designer", "Web Developer"]}
+                  className=''
+                  text={["Tester", "Developer"]}
                   speed={100}
                   eraseSpeed={100}
                 />
+                </div>
                 <div className="social-links">
                   <a href="https://www.instagram.com/raja_guru03/"><Instagram /></a>
                   <a href="https://github.com/Rajaguru03"><GitHub /> </a>
@@ -72,12 +75,12 @@ function Root() {
                     </div>
                   </div>
                 </div>
-                <p>Hey..!! I'm Rajaguru Nataraj</p>
+                <p>Hey! I'm Rajaguru Nataraj</p>
               </div>
               <div >
                 <div className="text-center content">
                   <div></div>
-                  <h3>Web Developer &amp; UI/UX Designer</h3>
+                  <h3>Web Developer &amp; Penetration Testing</h3>
                   <br />
                   <p>I am currently pursuing computer science and engineering at "SNS college of technology". Throughout my academics, I have had the opportunity to develop my skills and work experience, which have given me valuable experience and insights into web application Deveploment. I'm passionate about creating initiative and engaging user experiences through web design and development. I am always eager to learn new things and to take on new challenges, and I'm always looking for new challenges and stay up-to-date with the latest trends and technologies. I'm passionate about implementing my skills and experience to lead a healthy mission.</p>
                   <div data-aos="zoom-in" className='contentId my-5 d-flex align-item-center justify-content-around'>
