5994f608418ac4edea5fa217134d137ac40aea2e
Rajaguru03 <rajagurun003@gmail.com>
2024-09-29 00:54:29 +0530

    commit

--- src/WebUI/Root.jsx
+++ src/WebUI/Root.jsx
@@ -82,9 +82,9 @@ function Root() {
               <div >
                 <div className="text-center content">
                   <div></div>
-                  <h3>Web Developer &amp; Penetration Testing</h3>
+                  <h3>Web Craftsman &amp; Performance Validator</h3>
                   <br/>
-                  <p>A recent computer science and engineering graduate from the "SNS College of Technology". Throughout my academic career, I have had the opportunity to improve my abilities and gain professional experience, which has provided me with useful knowledge and insights into web application development. I'm passionate about taking the initiative and developing engaging user experiences through web design and development. I am always eager to learn new things and take on new challenges, and I am constantly on the lookout for new challenges and staying current with the latest trends and technologies. I am enthusiastic about using my abilities and knowledge to lead a healthy mission.</p>
+                  <p>A recent computer science and engineering graduate from the "SNS College of Technology". My academic journey was marked by actively honing my creativity, problem-solving and teamwork skills through numerous competitions and workshops. Driven by a passion for learning new technologies and staying ahead of industry trends. I am always eager to learn new things and take on new challenges, and I am constantly on the lookout for new challenges and staying current with the latest trends and technologies. I am enthusiastic about using my abilities and knowledge to lead a healthy mission.</p>
                   <div data-aos="zoom-in" className='contentId my-5 d-flex align-item-center justify-content-around'>
                     <ul>
                       <li><strong>Birthday:</strong><p>03 Nov 2002</p></li>
@@ -108,25 +108,25 @@ function Root() {
           <section id="skills" className="skills section-bg">
             <div className="container" >
               <div className="section-title">
-                <h2>Skills</h2>
+                <h2>Specializations</h2>
                 <p>I am passionate about my ability to learn and develop more skills, and am always seeking new challenges and opportunities to learn and grow. Please feel free to browse my portfolio to see some of my work and accomplishments.</p>
               </div>
               <div className="row skills-content">
                 <div className="col-lg-6">
                   <div className="progress">
-                    <span className="skill">React js<i className="val">90%</i></span>
+                    <span className="skill">Threat knowledge<i className="val">90%</i></span>
                     <div className="progress-bar-wrap">
                       <LinearProgress variant='determinate' color='inherit' value={90} />
                     </div>
                   </div>
                   <div className="progress">
-                    <span className="skill">Node js<i className="val">80%</i></span>
+                    <span className="skill">Burp suite (testing tool)<i className="val">90%</i></span>
                     <div className="progress-bar-wrap">
-                      <LinearProgress variant='determinate' color='inherit' value={80} />
+                      <LinearProgress variant='determinate' color='inherit' value={90} />
                     </div>
                   </div>
                   <div className="progress">
-                    <span className="skill">sql <i className="val">75%</i></span>
+                    <span className="skill">sql<i className="val">75%</i></span>
                     <div className="progress-bar-wrap">
                       <LinearProgress variant='determinate' color='inherit' value={75} />
                     </div>
@@ -134,13 +134,13 @@ function Root() {
                 </div>
                 <div className="col-lg-6">
                   <div className="progress">
-                    <span className="skill"> version control system (Git, GitHub) <i className="val">80%</i></span>
+                    <span className="skill"> React js<i className="val">80%</i></span>
                     <div className="progress-bar-wrap">
                       <LinearProgress variant='determinate' color='inherit' value={80} />
                     </div>
                   </div>
                   <div className="progress">
-                    <span className="skill">UI/UX Design <i className="val">90%</i></span>
+                    <span className="skill">Linux<i className="val">90%</i></span>
                     <LinearProgress variant='determinate' color='inherit' value={90} />
                   </div>
                   <div className="progress">
@@ -249,8 +249,8 @@ ensuring the delivery of exceptional, responsive web applications.</em></p>
                         <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,521.0016835830174C376.1290562159157,517.8887921683347,466.0731472004068,529.7835943286574,510.70327084640275,468.03025145048787C554.3714126377745,407.6079735673963,508.03601936045806,328.9844924480964,491.2728898941984,256.3432110539036C474.5976632858925,184.082847569629,479.9380746630129,96.60480741107993,416.23090153303,58.64404602377083C348.86323505073057,18.502131276798302,261.93793281208167,40.57373210992963,193.5410806939664,78.93577620505333C130.42746243093433,114.334589627462,98.30271207620316,179.96522072025542,76.75703585869454,249.04625023123273C51.97151888228291,328.5150500222984,13.704378332031375,421.85034740162234,66.52175969318436,486.19268352777647C119.04800174914682,550.1803526380478,217.28368757567262,524.383925680826,300,521.0016835830174"></path>
                       </svg>
                     </div>
-                    <h4>MERN Stack</h4>
-                    <p>“There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for”</p>
+                    <h4>Penetration Testing </h4>
+                    <p>“The art of thinking like a hacker to outsmart potential threats, ensuring that your defenses are as strong as your intentions”</p>
                   </div>
                 </div>
                 <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-left">
@@ -260,8 +260,8 @@ ensuring the delivery of exceptional, responsive web applications.</em></p>
                         <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,582.0697525312426C382.5290701553225,586.8405444964366,449.9789794690241,525.3245884688669,502.5850820975895,461.55621195738473C556.606425686781,396.0723002908107,615.8543463187945,314.28637112970534,586.6730223649479,234.56875336149918C558.9533121215079,158.8439757836574,454.9685369536778,164.00468322053177,381.49747125262974,130.76875717737553C312.15926192815925,99.40240125094834,248.97055460311594,18.661163978235184,179.8680185752513,50.54337015887873C110.5421016452524,82.52863877960104,119.82277516462835,180.83849132639028,109.12597500060166,256.43424936330496C100.08760227029461,320.3096726198365,92.17705696193138,384.0621239912766,124.79988738764834,439.7174275375508C164.83382741302287,508.01625554203684,220.96474134820875,577.5009287672846,300,582.0697525312426"></path>
                       </svg>
                     </div>
-                    <h4>UI & UX Design</h4>
-                    <p>“UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.”</p>
+                    <h4>MERN Stack</h4>
+                    <p>“There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for”</p>
                   </div>
                 </div>
               </div>
